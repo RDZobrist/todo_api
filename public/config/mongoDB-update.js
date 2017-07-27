3a43f9b7e0622c8a764b6b0407f8ad7ee2d9a587
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-27 04:49:30 -0700

    wrote mongo-update.js

--- public/config/mongoDB-update.js
+++ public/config/mongoDB-update.js
@@ -1,28 +1,48 @@
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 
 
 
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-        return console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected To MongoDB server');
+            if (err) {
+                return console.log('Unable to connect to MongoDB server');
+            }
+            console.log('Connected To MongoDB server');
+// find entry in User collection with a specified id, update name, and change age by inceementing by one
+            db.collection('User').findOneAndUpdate({
+                    _id: new ObjectID("597953765f687b1438245677")
+                }, {
+                    $set: {
+                        name: "Robert D Zobrist"
+                    },
+                    $inc: {
+                        age: 1
 
-    db.collection('Todos').findOneAndUpdate({
-    	_id: new ObjectID("59795a70e80fbc0e237e35b0")
-    }, 
-     {
-    	$set: {
-    		completed: true
-    	}
+                    }
 
-    },
+                },
 
-    {
-    		returnOriginal: false
-    	}).then((result)=>{
-    	console.log(result)
-    })
-    });
+                {
+                    returnOriginal: false
+                }).then((result) => {
+                console.log(result)
+            })
+
+});
+
+            //     db.collection('Todos').findOneAndUpdate({
+            //             _id: new ObjectID("59795a70e80fbc0e237e35b0")
+            //         }, {
+            //             $set: {
+            //                 completed: true
+            //             }
+
+            //         },
+
+            //         {
+            //             returnOriginal: false
+            //         }).then((result) => {
+            //         console.log(result)
+            //     })
+            // });
