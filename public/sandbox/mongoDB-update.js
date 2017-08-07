9d56d9b80f6f56e27729f0fb2e70fc0efe4dd1f7
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-08-07 00:01:13 -0700

    added mongoose orm, the begining.

--- public/sandbox/mongoDB-update.js
+++ public/sandbox/mongoDB-update.js
@@ -9,10 +9,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
                 return console.log('Unable to connect to MongoDB server');
             }
             console.log('Connected To MongoDB server');
-            
-// find entry in User collection with a specified id, update name, and change age by inceementing by one
+
+// find entry in User collection with a specified id, update name, and change age by incrementing by one
             db.collection('User').findOneAndUpdate({
-                    _id: new ObjectID("597953765f687b1438245677")
+                    _id: new ObjectID("5987e6058628f5245424318d")
                 }, {
                     $set: {
                         name: "Robert D Zobrist"
@@ -32,18 +32,4 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
 });
 
-            //     db.collection('Todos').findOneAndUpdate({
-            //             _id: new ObjectID("59795a70e80fbc0e237e35b0")
-            //         }, {
-            //             $set: {
-            //                 completed: true
-            //             }
-
-            //         },
-
-            //         {
-            //             returnOriginal: false
-            //         }).then((result) => {
-            //         console.log(result)
-            //     })
-            // });
+            
